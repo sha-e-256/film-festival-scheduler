7cc203c6d6a4af574553485dc4a53023181bf30a
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-10-23 15:20:37 -0400

    Determine film length by clicking 'MORE INFO'
    - Obtain string containing length of a film by
    parsing the data saved in the 'film-extras'
    class which can be found by clicking 'MORE INFO'
    in the 'film-content' class
    - Determine a way to return to the film listings
    page after clicking 'MORE INFO' so that the film
    lengths of all movies can be determined

--- web_scraper.py
+++ web_scraper.py
@@ -4,18 +4,33 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from webdriver_manager.firefox import GeckoDriverManager
 
-browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-browser.get('https://windsorfilmfestival.com/')
-filmList_button = browser.find_element(by=By.PARTIAL_LINK_TEXT,
+
+def main() -> int:
+    browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    browser.get('https://windsorfilmfestival.com/')
+    
+    
+    filmList_button = browser.find_element(by=By.PARTIAL_LINK_TEXT,
                                        value='Films & Tickets')
-filmList_button.click()
-# After clicking, wait 1 s (1000 ms) for the site to load
-# Locator accepts a tuple as an arguement
-filmData_list = WebDriverWait(browser, 1000).until(expected_conditions.presence_of_all_elements_located(locator=(By.XPATH,
-                                                                                   "//div[@class='film-content']")))
-for filmData in filmData_list:
-    filmName = filmData.find_element(by=By.XPATH,
+    filmList_button.click()
+
+    # After clicking, wait 2 s (5000 ms) for the site to load
+    # Locator accepts a tuple as an arguement
+    filmData_list = WebDriverWait(browser, 2000).until(expected_conditions.presence_of_all_elements_located(locator=(By.XPATH,
+                                                                                                            "//div[@class='film-content']")))
+    for filmData in filmData_list:
+        filmName = filmData.find_element(by=By.XPATH,
                                      value="./h3") # . Means to search
                                                    # the child of the current node
-    print(filmName.text)
-browser.quit()
+        
+        moreInfo_button = filmData.find_element(by=By.PARTIAL_LINK_TEXT,
+                                               value='MORE INFO')
+        moreInfo_button.click()
+        film_extras = WebDriverWait(browser, 5000).until(expected_conditions.presence_of_all_elements_located(locator=(By.XPATH,
+                                                                                                            "//div[@class='film-extras']")))
+        film_extras_str = film_extras[0].text
+        film_length = film_extras_str.split('\n')[-1]
+    browser.quit()
+
+if __name__ == '__main__':
+    main()
