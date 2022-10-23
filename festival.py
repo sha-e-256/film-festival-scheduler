e7acdd17692741867586e1287cb89671ff3a02c7
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-10-23 14:13:07 -0400

    Create festival package
    - Create Film class and place in festival package
    - Create Screening class and place in festival package
    - Create constructors for Film and Screening classes
    - Add docstrings for classes and constructors
    - Add typehints for constructor methods

---
+++ festival.py
@@ -0,0 +1,72 @@
+import time
+from datetime import date
+import typing
+
+
+class Screening:
+    '''
+    This class stores information on a screening.
+
+    Attributes:
+    -----------
+    film_time: str
+        The time of the screening.
+    film_date: date
+        The date of the screening.
+    film_locaton: str
+        The location of the screening.
+    '''
+    def __init__(self, screening_time: str,
+                 screening_date: date,
+                 screening_location: str) -> None:
+        '''
+        The constructor of the Screening class.
+
+        Parameters:
+        ----------
+        film_time: str
+            The time of the screening.
+        film_date: date
+            The date of the screening.
+        film_locaton: str
+            The location of the screening.
+        '''
+        self.screening_time = screening_time
+        self.screnning_date = screening_date
+        self.screening_location = screening_location
+
+
+class Film:
+    '''
+    This called stores information on a film.
+
+    Attributes:
+    -----------
+    film_name: str
+        Name of the film.
+    film_screenings: List[Screening])
+        A list of the available screenings where each
+        Screening object in the list contains information on the screening such
+        as the time, date, and location.
+    film_length: int
+        The length of a film in minutes.
+    '''
+    def __init__(self, film_name: str,
+                 film_screenings: [Screening],
+                 film_length: int) -> None:
+        '''
+        The constructor for the Film class
+
+        Parameters:
+        ----------
+        film_name: str
+            Name of the film.
+        film_screenings: List[Screening])
+            A list of the available screenings where each
+            Screening object in the list contains information on the screening
+            such as the time, date, and location.
+        film_length: int
+        '''
+        self.film_name = film_name
+        self.film_screenings = film_screenings
+        self.film_length = film_length
--- web_scraper.py
+++ web_scraper.py
@@ -9,13 +9,13 @@ browser.get('https://windsorfilmfestival.com/')
 filmList_button = browser.find_element(by=By.PARTIAL_LINK_TEXT,
                                        value='Films & Tickets')
 filmList_button.click()
-# After clicking, wait 5 s (5000 ms) for the site to load
+# After clicking, wait 1 s (1000 ms) for the site to load
 # Locator accepts a tuple as an arguement
-filmData_list = WebDriverWait(browser, 5000).until(expected_conditions.presence_of_all_elements_located(locator=(By.XPATH, 
+filmData_list = WebDriverWait(browser, 1000).until(expected_conditions.presence_of_all_elements_located(locator=(By.XPATH,
                                                                                    "//div[@class='film-content']")))
 for filmData in filmData_list:
     filmName = filmData.find_element(by=By.XPATH,
-                                     value="./h3") # . Means to search 
+                                     value="./h3") # . Means to search
                                                    # the child of the current node
     print(filmName.text)
 browser.quit()
