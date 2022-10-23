827a7046682603906e801b289bc69c4110724eb6
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-10-23 01:02:35 -0400

    Navigate to page containing list of films
    - Click on 'Films & Tickets' element on DOM to
    navigate to the page contaning the list of films
    - Add WebDriverWait to wait for the page to load
    after clicking on the button to navigate to the
    page containing the list of films
    - Find list of films using xpath
    - Add geckodriver.log to .gitignore

--- web_scraper.py
+++ web_scraper.py
@@ -1,10 +1,28 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 from webdriver_manager.firefox import GeckoDriverManager
 
 browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 browser.get('https://windsorfilmfestival.com/')
-browser.quit()
+filmList_button = browser.find_element(by=By.PARTIAL_LINK_TEXT,
+                                       value='Films & Tickets')
+filmList_button.click()
+# After clicking, wait 1 s (1000 ms) for the site to load
+# WebDriverWait(browser, 1000).until(expected_conditions.presence_of_element_located(by=By.CLASS_NAME, 
+#                                                                                   value='festival-schedule-films-list'))
+print('found')
+'''
+try:
+    filmNames = browser.find_elements(by=By.XPATH, 
+                                      value="//div[@class='festival-schedule-films-list-item']")
+    print(*fileNames)
+except :
+    print('Unable to locate element')
+'''
+# browser.quit()
 
 # Check if file exists in directory 
 wiffSite_filename = 'wiffSite_data.txt'
