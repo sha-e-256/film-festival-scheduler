7ebf5eb98dd3fd1e4a2656468bb00871d43ee1b8
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-10-24 17:45:44 -0400

    Save scrapped data to a .dat file using pickle
    - Save all the film objects to a .dat file w/
    pickle; this is done to be able to
    quickly access the scrapped data after a succesful
    scrape without having to scrape the website again
    - Delete 'wiff_site.txt' file

--- web_scraper.py
+++ web_scraper.py
@@ -5,72 +5,92 @@ from selenium.webdriver.support import expected_conditions
 from webdriver_manager.firefox import GeckoDriverManager
 from festival import Film, Screening
 from datetime import date, datetime, time, timedelta
+import pickle
+import os
 
+def save_film_objects(film_objects, dst_file_name) -> None:
 
-def main() -> int:    
+    with open(file=dst_file_name, mode="wb") as file:
+        pickle.dump(obj=film_objects, file=file)
+        file.close()
 
-    film_list = []  # Create a list of Film objects
-    screening_list = []  # Create a list of Screening objects
-    browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-    browser.get('https://windsorfilmfestival.com/')
-    film_list_button = browser.find_element(by=By.PARTIAL_LINK_TEXT,
-                                       value='Films & Tickets')
-    film_list_button.click()
+def load_film_objects(src_file_name):
 
-    # After clicking, wait 2 s (5000 ms) for the site to load
-    # Locator accepts a tuple as an arguement
-    film_data_list = WebDriverWait(browser, 2000).until(expected_conditions.presence_of_all_elements_located(locator=(By.XPATH,
-                                                                                                                "//div[@class='film-content']")))
-    for film_data in film_data_list:
-        film_name_element = film_data.find_element(by=By.XPATH,
-                                         value="./h3")  # . Means to search
-                                                        # the child of the current node
-        film_name = film_name_element.text
-        film_screening_list = film_data.find_elements(by=By.XPATH,
-                                                      value=".//div[@class='film-screen']")
-        for film_screening in film_screening_list:
+    with open(file=src_file_name, mode="rb") as file:
+        film_objects = pickle.load(file=file)
+    file.close()
+    return film_objects
+
+def main() -> None:   
+
+    file_name = 'film_objects.dat'
+    film_objects_file_exists = os.path.exists(file_name)
+    if not film_objects_file_exists:
+        film_list = []  # Create a list of Film objects
+        screening_list = []  # Create a list of Screening objects
+        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        browser.get('https://windsorfilmfestival.com/')
+        film_list_button = browser.find_element(by=By.PARTIAL_LINK_TEXT,
+                                        value='Films & Tickets')
+        film_list_button.click()
+
+        # After clicking, wait 2 s (5000 ms) for the site to load
+        # Locator accepts a tuple as an arguement
+        film_data_list = WebDriverWait(browser, 2000).until(expected_conditions.presence_of_all_elements_located(locator=(By.XPATH,
+                                                                                                                    "//div[@class='film-content']")))
+        for film_data in film_data_list:
+            film_name_element = film_data.find_element(by=By.XPATH,
+                                            value="./h3")  # . Means to search
+                                                            # the child of the current node
+            film_name = film_name_element.text
+            film_screening_list = film_data.find_elements(by=By.XPATH,
+                                                        value=".//div[@class='film-screen']")
+            for film_screening in film_screening_list:
+                
+                film_screening_str = film_screening.text
+                film_screen_str_list = film_screening_str.split('\n')
+
+                film_start_time_str = film_screen_str_list[0]
+                film_start_time = datetime.strptime(film_start_time_str, '%I:%M %p')
+                
+                
+                film_length = 100  # Filler for now
+                film_end_time = film_start_time + timedelta(minutes=film_length)
+                
+                film_date_str = film_screen_str_list[1]
+                film_date = datetime.strptime(film_date_str, 
+                                            '%a %b %d').date()  # Year defaults to 1900
+                film_date_correct_year = film_date.replace(2022)  # Change year to 2022
+                
+                film_location = film_screen_str_list[2]
+
+                # Make a Screening object
+                curr_screening = Screening(screening_time_start=film_start_time,
+                                        screening_time_end=film_end_time,
+                                        screening_date=film_date_correct_year,
+                                        screening_location=film_location)
+                screening_list.append(curr_screening)
+
+            # Make a Film object
+            curr_film = Film(film_name=film_name, film_screenings=screening_list)
+            film_list.append(curr_film)
+            screening_list = []  # Empty the list of screenings 
             
-            film_screening_str = film_screening.text
-            film_screen_str_list = film_screening_str.split('\n')
+            save_film_objects(film_objects=film_list, dst_file_name=dst_file_name)
+            '''
+            moreInfo_button = filmData.find_element(by=By.PARTIAL_LINK_TEXT,
+                                                    value='MORE INFO')
+            moreInfo_button.click()
+            film_extras = WebDriverWait(browser, 5000).until(expected_conditions.presence_of_all_elements_located(locator=(By.XPATH,
+                                                                                                                "//div[@class='film-extras']")))
+            film_extras_str = film_extras[0].text
+            film_length = film_extras_str.split('\n')[-1]
+            '''
+        browser.quit()
 
-            film_start_time_str = film_screen_str_list[0]
-            film_start_time = datetime.strptime(film_start_time_str, '%I:%M %p')
-            
-            
-            film_length = 100  # Filler for now
-            film_end_time = film_start_time + timedelta(minutes=film_length)
-            
-            film_date_str = film_screen_str_list[1]
-            film_date = datetime.strptime(film_date_str, 
-                                         '%a %b %d').date()  # Year defaults to 1900
-            film_date_correct_year = film_date.replace(2022)  # Change year to 2022
-            
-            film_location = film_screen_str_list[2]
-
-            # Make a Screening object
-            curr_screening = Screening(screening_time_start=film_start_time,
-                                       screening_time_end=film_end_time,
-                                       screening_date=film_date_correct_year,
-                                       screening_location=film_location)
-            screening_list.append(curr_screening)
-
-        # Make a Film object
-        curr_film = Film(film_name=film_name, film_screenings=screening_list)
-        film_list.append(curr_film)
-        screening_list = []  # Empty the list of screenings 
-
-        '''
-        moreInfo_button = filmData.find_element(by=By.PARTIAL_LINK_TEXT,
-                                                value='MORE INFO')
-        moreInfo_button.click()
-        film_extras = WebDriverWait(browser, 5000).until(expected_conditions.presence_of_all_elements_located(locator=(By.XPATH,
-                                                                                                            "//div[@class='film-extras']")))
-        film_extras_str = film_extras[0].text
-        film_length = film_extras_str.split('\n')[-1]
-        '''
-    browser.quit()
-    for film in film_list:
-        print(film)
+    film_objects = load_film_objects(file_name)
+    for film_object in film_objects:
+        print(film_object)
 
 if __name__ == '__main__':
     main()
