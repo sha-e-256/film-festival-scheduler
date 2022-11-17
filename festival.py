c0dfd510fc569dca214b7e8e1e913ab86ae1bccd
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-11-17 15:11:55 -0500

    Correct format of docstrings using 'pydocstyle'
    - Correct format of docstrings using the
    'pydocstyle' analysis tool
    - Correct type of 'film_screening' variable from
    List[Screening] to defaultdict(set)

--- festival.py
+++ festival.py
@@ -1,39 +1,35 @@
+"""This package contains classes used to store information on films.
+
+A Film object stores the name of a film and a list of Screening objects. A
+Screening object stores information on a particular screening such as start/end
+time, date, and location. This package is used in the 'scheduler' module which
+is the module used to develop a schedule for a user.
+"""
+from collections import defaultdict
 from datetime import date, datetime
 from textwrap import dedent
-from typing import List
 
-'''
-This package contains two classes used to store information
-on films: Film and Screening. A Film object stores the name of a film,
-and a list of Screening objects. A Screening object stores information on
-a particular screening such as start/end time, date, and location.
-This package is used in the 'scheduler' module which is the module used to
-develop a schedule for a user.
-'''
 
 class Screening:
-    '''
-    This class stores information on a screening.
+    """This class stores information on a screening.
 
     Attributes:
     -----------
-    screening_time_start: time
+    screening_time_start: datetime
         The time the screening starts.
-    screening_time_end: time
+    screening_time_end: datetime
         The time the screening ends.
     screening_date: date
         The date of the screening.
     screening_locaton: str
         The location of the screening.
-    '''
-
+    """
 
     def __init__(self, screening_time_start: datetime,
                  screening_time_end: datetime,
                  screening_date: date,
                  screening_location: str) -> None:
-        '''
-        The constructor of the Screening class.
+        """Create an object of the Screening class.
 
         Parameters:
         ----------
@@ -45,7 +41,7 @@ class Screening:
             The date of the screening.
         film_locaton: str
             The location of the screening.
-        '''
+        """
         self.screening_time_start = screening_time_start
         self.screening_time_end = screening_time_end
         self.screening_date = screening_date
@@ -53,14 +49,13 @@ class Screening:
 
 
     def __str__(self) -> str:
-        '''
-        Creates a well-formatted string that displays the information
-        on a screening.
+        """Return a string that displays the information on a Screening object.
 
-            Returns:
-                text (str): A well-formatted string that displays information
-                on a Screening object.
-        '''
+        Returns:
+        --------
+            text: str
+                A string that displays the information on a Screening object.
+        """
         text = dedent(f'''\
         -> Start Time: {self.screening_time_start.time()}
         -> End Time: {self.screening_time_end.time()}
@@ -71,50 +66,41 @@ class Screening:
 
 
 class Film:
-    '''
-    This class stores information on a film.
+    """This class stores information on a film.
 
     Attributes:
     -----------
     film_name: str
-        Name of the film.
-    film_screenings: List[Screening])
-        A list of the available screenings where each
-        Screening object in the list contains information on the screening such
-        as the time, date, and location.
-    '''
-
+        The name of the film.
+    film_screenings: defaultdict(set)
+        A dict of all the Screening objects for the particular film.
+    """
 
     def __init__(self, film_name: str,
-                 film_screenings: List[Screening]) -> None:
-        '''
-        The constructor for the Film class
+                 film_screenings: defaultdict(set)) -> None:
+        """Create an object of the Film class.
 
         Parameters:
         ----------
         film_name: str
             Name of the film.
-        film_screenings: List[Screening])
-            A list of the available screenings where each
-            Screening object in the list contains information on the screening
-            such as the time, date, and location.
-        '''
+        film_screenings: defaultdict(set)
+            A dict of all the Screening objects for the particular film.
+        """
         self.film_name = film_name
         self.film_screenings = film_screenings
 
 
-
     def __str__(self) -> str:
-        '''
-        Creates a well-formatted string that displays the information on
-        a film.
+        """Return a string that displays the information on a Film object.
 
-            Returns:
-                text (str): a well-formatted string that diisplays information
-                on a Film object.
-        '''
+        Returns:
+        --------
+            text : str
+                A string that displays the information on a Film object.
+        """
         text = f'\nFilm Name: {self.film_name}\nList of Screenings:\n'
         # List comprehension using a string
         text = text.join(screening.__str__() for screening in \
-            self.film_screenings)
+            self.film_screenings.values())
         return text
